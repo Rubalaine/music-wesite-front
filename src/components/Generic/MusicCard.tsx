9f55ec1ed992f22e27548bfe26a62e66d03793f4
Rubalaine <kelvenizzy@gmail.com>
2021-04-02 16:38:43 +0200

    new todos added for memorization

--- src/components/Generic/MusicCard.tsx
+++ src/components/Generic/MusicCard.tsx
@@ -13,12 +13,15 @@ const Card = styled.div`
   font-size: 1.6rem;
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
 `;
+/**
+ * TODO:
+ * Make a card like spotify
+ * with blur animation on hover
+ * and show a button while hover
+ *
+ */
 const MusicCard = ({ title, artist, imageUrl }: Props) => {
-  return (
-    <Card>
-      some
-    </Card>
-  );
+  return <Card>some</Card>;
 };
 
 export default MusicCard;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import MusicCard from "../components/Generic/MusicCard";
-
+/**
+ * TODO:
+ * will be divided in part
+ * the main part will ocupy 70% of the usable space
+ * the 30% percent of the space will be reserved for the top charts
+ * whill have
+ * top musics
+ * and top albums
+ *
+ *  */
 const Home = () => {
   return <MusicCard imageUrl="urls" title="tile" artist="artist" />;
 };
