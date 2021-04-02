9559f8057e33e5dcfa9747d7b891fac1a0620558
Rubalaine <kelvenizzy@gmail.com>
2021-04-03 01:12:20 +0200

    feita tipagem para os dados provenientes das requests

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,37 @@
 /// <reference types="react-scripts" />
+interface IArtista {
+  id: number;
+  biografia: string;
+  nome: string;
+  created_at: string;
+  updated_t: string;
+}
+interface ICoverImage {
+  url: string;
+  ext: string;
+  id: number;
+  created_at: string;
+  updated_t: string;
+}
+interface IGenero {
+  id: number;
+  nome: string;
+  created_at: string;
+  updated_t: string;
+}
+interface IProjectsResponse {
+  id: string;
+  artista: IArtista;
+  categoria: string;
+  cover: ICoverImage;
+  descricao: string;
+  destaque: true | false;
+  formato: string;
+  generos: IGenero[];
+  lancamento: string;
+  participantes: IArtista[];
+  tags: string;
+  tamanho: number;
+  titulo: string;
+  url: string;
+}
